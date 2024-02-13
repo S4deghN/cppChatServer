55429101a9ca4be4b618497bc58b948cfca9e6f6
s4 <sadegh.nobakhti@gmail.com>
2024-02-14 01:25:11 +0330

    finaly! multithreaded execution works. memory consumption and allocations is abysmal

--- src/client.cpp
+++ src/client.cpp
@@ -1,3 +1,4 @@
+#include <boost/asio/detached.hpp>
 #include <chrono>
 #include <ctime>
 #if defined(__clang__)
@@ -19,9 +20,9 @@
 
 namespace io = boost::asio;
 using tcp = io::ip::tcp;
-using ssl_socket = io::ssl::stream<tcp::socket&>;
+using ssl_socket = io::ssl::stream<tcp::socket>;
 
-std::string time_in_string() {
+std::string time_in_string() {// {{{
     using namespace std::chrono;
 
     // get current time
@@ -40,35 +41,37 @@ std::string time_in_string() {
     std::ostringstream oss;
 
     oss << std::put_time(&bt, "%H:%M:%S");  // HH:MM:SS
-    oss << '.' << std::setfill('0') << std::setw(6) << ms.count();
+    oss << '.' << std::setfill('0') << std::setw(6) << ms.count() << ": ";
 
     return oss.str();
-}
+}// }}}
 
 class Client {
 private:
     io::io_context& io_context;
-    tcp::socket socket;
-    tcp::endpoint endpoint;
-    ssl_socket ssl_socket;
     io::ssl::context& ssl_context;
+    ssl_socket ssl_socket;
+    tcp::endpoint endpoint;
+
     io::posix::stream_descriptor input_descriptor;
     io::posix::stream_descriptor output_descriptor;
+
+    std::string stdio_read_buffer;
+    std::string stdio_write_buffer;
+    std::string socket_read_buffer;
 
 public:
     Client(io::io_context& io_context, io::ssl::context& ssl_context, tcp::socket&& socket, tcp::endpoint&& endpoint) :
         io_context(io_context),
-        socket(std::move(socket)),
-        endpoint(std::move(endpoint)),
         ssl_context(ssl_context),
-        ssl_socket(this->socket, ssl_context),
+        ssl_socket(std::move(socket), ssl_context),
+        endpoint(std::move(endpoint)),
         input_descriptor(io_context, dup(STDIN_FILENO)),
         output_descriptor(io_context, dup(STDOUT_FILENO)) {
-        io::co_spawn(io_context, connect(), io::detached);
     }
 
     io::awaitable<void> connect() {
-        auto [err] = co_await socket.async_connect(endpoint, io::as_tuple(io::use_awaitable));
+        auto [err] = co_await ssl_socket.next_layer().async_connect(endpoint, io::as_tuple(io::use_awaitable));
         if (err) {
             spdlog::error("connect: {}", err.what());
             co_return;
@@ -84,63 +87,56 @@ public:
         }
 
         spdlog::info("Connected!");
-        co_spawn(io_context, do_read_stdio(), io::detached);
-        co_spawn(io_context, do_read_socket(), io::detached);
+        co_spawn(io_context, read_stdio(), io::detached);
+        co_spawn(io_context, read_socket(), io::detached);
     }
 
-    io::awaitable<void> do_read_stdio() {
-        for (std::string read_buff;;) {
-            auto [err, n] = co_await boost::asio::async_read_until(input_descriptor, io::dynamic_buffer(read_buff),
-                                                                   '\n', io::as_tuple(io::use_awaitable));
-            if (err) {
-                spdlog::error("do_read_stdio: {}", err.what());
-                co_return;
+    io::awaitable<void> read_stdio() {
+        for (;;)
+        {
+            try
+            {
+                auto n = co_await boost::asio::async_read_until(input_descriptor, io::dynamic_buffer(stdio_read_buffer, 254), '\n', io::use_awaitable);
+                co_spawn(io_context, write_socket(stdio_read_buffer), io::detached);
+                stdio_read_buffer.erase(0, n);
             }
-
-            co_spawn(io_context, write_socket(std::move(read_buff)), io::detached);
-        }
-    }
-
-    io::awaitable<void> write_stdio(std::string msg) {
-        std::string str = time_in_string();
-        str.append(": ");
-        str.append(msg);
-
-        auto [err, n] = co_await io::async_write(output_descriptor, io::buffer(str), io::as_tuple(io::use_awaitable));
-        if (err) {
-            spdlog::error("write_stdio: {}", err.what());
-            co_return;
-        }
-    }
-
-    io::awaitable<void> do_read_socket() {
-        for (std::string read_buff;;) {
-            auto [err, n] = co_await io::async_read_until(ssl_socket, io::dynamic_buffer(read_buff, 512), '\n',
-                                                          io::as_tuple(io::use_awaitable));
-            if (err) {
-                spdlog::error("do_read_socket: {}", err.what());
-                terminate();
-                co_return;
+            catch (std::exception& err)
+            {
+                spdlog::error("stdio_to_socket: {}", err.what());
+                close();
             }
-
-            spdlog::info("{} bytes: {}", n, read_buff.substr(0, n - 1));
-            co_spawn(io_context, write_stdio(std::move(read_buff)), io::detached);
         }
     }
 
     io::awaitable<void> write_socket(std::string msg) {
-        auto [err, n] = co_await io::async_write(ssl_socket, io::buffer(msg), io::as_tuple(io::use_awaitable));
-        if (err) {
-            spdlog::error("write_socket: {}", err.what());
-            terminate();
-            co_return;
+        co_await io::async_write(ssl_socket, io::buffer(msg), io::use_awaitable);
+    }
+
+    io::awaitable<void> read_socket() {
+        for (;;)
+        {
+            try
+            {
+                auto n = co_await io::async_read_until(ssl_socket, io::dynamic_buffer(socket_read_buffer, 254), '\n', io::use_awaitable);
+                co_spawn( io_context, write_stdio(std::move(time_in_string().append(socket_read_buffer))), io::detached);
+                socket_read_buffer.erase(0, n);
+            }
+            catch (std::exception& err)
+            {
+                spdlog::error("socket_to_stdio: {}", err.what());
+                close();
+            }
         }
     }
 
-    void terminate() {
-        socket.close();
+    io::awaitable<void> write_stdio(std::string&& msg) {
+        co_await io::async_write(output_descriptor, io::buffer(msg), io::use_awaitable);
+    }
+
+    void close() {
+        ssl_socket.next_layer().close();
         io_context.stop();
-        spdlog::error("Connection closed due to an error!");
+        spdlog::error("Connection closed!");
     }
 };
 
@@ -148,13 +144,14 @@ int main() {
     spdlog::set_pattern("[%H:%M:%S:%f] [%^%l%$]\t%v");
     spdlog::set_level(spdlog::level::err);
 
-    io::io_context io_context;
+    io::io_context io_context(1);
     io::ssl::context ssl_context(io::ssl::context::tlsv13_client);
     ssl_context.set_options(io::ssl::context::tlsv13);
     ssl_context.load_verify_file("../data/server.crt");
     ssl_context.set_verify_mode(io::ssl::context::verify_peer);
 
     Client client(io_context, ssl_context, tcp::socket(io_context), tcp::endpoint(tcp::v4(), 55555));
+    co_spawn(io_context, client.connect(), io::detached);
     io_context.run();
 
     return 0;
--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,4 @@
+#include <boost/asio/bind_executor.hpp>
 #if defined(__clang__)
     #define _LIBCPP_ENABLE_CXX20_REMOVED_TYPE_TRAITS
 #endif
@@ -21,6 +22,8 @@ using ssl_socket = io::ssl::stream<tcp::socket>;
 
 class Session;
 using Session_ptr = std::shared_ptr<Session>;
+
+#define on_error(e) on_error_with_info(e, __PRETTY_FUNCTION__)
 
 class Server {
 public:
@@ -65,37 +68,49 @@ public:
     io::steady_timer timer;
     std::string read_buffer;
     safe_deque<std::string> write_q;
+    io::strand<io::any_io_executor> io_strand;
 
     Session(tcp::socket&& socket, Server& server)
         : server(server)
         , ssl_socket(std::move(socket), server.ssl_context)
         , timer(server.io_context)
+        , io_strand(io::make_strand(ssl_socket.get_executor()))
     {
         timer.expires_at(std::chrono::steady_clock::time_point::max());
     }
 
-    io::awaitable<boost::system::error_code> handshake() {
+    io::awaitable<void> handshake() {
         ssl_socket.next_layer().set_option(tcp::no_delay(true));
+
         auto [err] = co_await ssl_socket.async_handshake(io::ssl::stream_base::server, io::as_tuple(io::use_awaitable));
-        co_return err;
+        if (err) {
+            on_error(err);
+            co_return;
+        }
+
+        io::dispatch(server.sessions_strand, [self = shared_from_this()] { self->server.sessions.emplace(self); });
+
+        io::co_spawn(io_strand, [self = shared_from_this()] { return self->do_read(); }, io::detached);
+        io::co_spawn(io_strand, [self = shared_from_this()] { return self->do_write(); }, io::detached);
+
+        // for (auto const& session : server.sessions) {
+        //     spdlog::info("Session : {}, refs: {}", (size_t)session.get(), session.use_count());
+        // }
     }
 
     io::awaitable<void> do_read() {
         for (;;) {
-            auto [err, n] = co_await io::async_read_until(ssl_socket, io::dynamic_buffer(read_buffer, 254), '\n', io::as_tuple(io::use_awaitable));
-
-            // spdlog::info("{} bytes: {}", n, read_buffer.substr(0, n - 1));
-
+            auto [err, n] =
+                co_await io::async_read_until(ssl_socket, io::dynamic_buffer(read_buffer, 254), '\n',
+                                              io::as_tuple(io::bind_executor(io_strand, io::use_awaitable)));
             if (err) {
-                if (err == io::error::operation_aborted) {
-                    co_return;
-                }
-                close();
-                spdlog::error("{}: {}", __PRETTY_FUNCTION__, err.what());
+                on_error(err);
                 co_return;
             }
 
-            io::post(server.sessions_strand,
+            // spdlog::info("{} bytes: {}", n, read_buffer.substr(0, n - 1));
+
+            io::dispatch(server.sessions_strand,
                     [self = shared_from_this(), msg = read_buffer.substr(0, n)] { self->server.post_msg(msg); });
             read_buffer.erase(0, n);
         }
@@ -107,23 +122,24 @@ public:
                 co_return;
             }
             if (write_q.size()) {
-                auto [err, n] = co_await io::async_write(ssl_socket, io::buffer(write_q.front()), io::as_tuple(io::use_awaitable));
-                write_q.pop_front();
+                auto [err, n] = co_await io::async_write(ssl_socket, io::buffer(write_q.front()),
+                                                         io::as_tuple(io::bind_executor(io_strand, io::use_awaitable)));
                 if (err) {
-                    if (err == io::error::operation_aborted) {
-                        co_return;
-                    }
-                    spdlog::error("{}: {}", __PRETTY_FUNCTION__, err.what());
-                    close();
+                    on_error(err);
                     co_return;
                 }
+                write_q.pop_front();
             } else {
-                auto [err] = co_await timer.async_wait(io::as_tuple(io::use_awaitable));
+                auto [err] = co_await timer.async_wait(io::as_tuple(io::bind_executor(io_strand, io::use_awaitable)));
             }
         }
     }
 
-    void close() {
+    void on_error_with_info(boost::system::error_code err, const char* func) {
+        if (err == io::error::operation_aborted) {
+            return;
+        }
+        spdlog::error("{}: {}", func, err.what());
         ssl_socket.next_layer().close();
         timer.cancel();
         io::dispatch(server.sessions_strand, [self = shared_from_this()] { self->server.sessions.erase(self); });
@@ -142,23 +158,7 @@ io::awaitable<void> Server::do_accept(tcp::acceptor& acceptor) {
     spdlog::info("Accepting connection on: {}:{}", acceptor.local_endpoint().address().to_string(), acceptor.local_endpoint().port());
     for (;;) {
         auto session = std::make_shared<Session>(co_await acceptor.async_accept(io::use_awaitable), *this);
-
-        io::co_spawn(sessions_strand,
-                [session] { return session->handshake(); },
-                [&, session] (const std::exception_ptr& /*exp*/, boost::system::error_code err) {
-                    if (err) {
-                        spdlog::error("{}: {}", __PRETTY_FUNCTION__, err.what());
-                        return;
-                    }
-
-                    sessions.emplace(session);
-                    spdlog::info("Sessions : {}", sessions.size());
-
-                    io::co_spawn(io_context, [session] { return session->do_read(); }, io::detached);
-                    io::co_spawn(io_context, [session] { return session->do_write(); }, io::detached);
-                    return;
-                });
-
+        io::co_spawn(io_context, [session] { return session->handshake(); }, io::detached);
     }
 }
 
